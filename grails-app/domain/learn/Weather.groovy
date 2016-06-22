b5f3604e9bfe371162a16fc80f076b85729e38cf
Cristiana Stan <cristiana.stan@Mac-mini.local>
2016-06-22 11:24:54 +0300

    weather app

---
+++ grails-app/domain/learn/Weather.groovy
@@ -0,0 +1,14 @@
+package learn
+
+class Weather {
+
+    String location
+    String country
+    Double temp
+    Double humidity
+    String description
+    Date lastUpdated
+
+    static constraints = {
+    }
+}
